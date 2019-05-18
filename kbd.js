e8978ec3cd2ac22f08de2bb207c7134fb5e43235
tarboh <50641757+tarboh@users.noreply.github.com>
2019-05-18 18:22:42 +0900

    Update kbd.js

--- kbd.js
+++ kbd.js
@@ -5,17 +5,7 @@ var tbl=document.getElementById('data_tbl').getElementsByTagName('tr');
 
 /*ループで回す*/
 for(var i=1;i<=51;i++){
-	switch(tbl.item(i).getElementsByTagName('a').item(0).href.slice(-1)){
-		case 0:alert('bSP');break;
-		case 1:alert('BSP');break;
-		case 2:alert('DSP');break;
-		case 3:alert('ESP');break;
-		case 4:alert('CSP');break;
-		case 5:alert('BDP');break;
-		case 6:alert('DDP');break;
-		case 7:alert('EDP');break;
-		case 8:alert('CDP');break;
-	}
+	alert(tbl.item(i).getElementsByTagName('a').item(0).href.slice(-1));
 	
 }
 //item(i).getElementsByTagName('a').item(0).href.slice(-1)
