b1525d8b8e265ffe0dbdeb68edd7aa7379cd7355
tarboh <50641757+tarboh@users.noreply.github.com>
2019-05-18 18:25:09 +0900

    Update kbd.js

--- kbd.js
+++ kbd.js
@@ -5,7 +5,7 @@ var tbl=document.getElementById('data_tbl').getElementsByTagName('tr');
 
 /*ループで回す*/
 for(var i=1;i<=51;i++){
-	alert(tbl.item(i).getElementsByTagName('a').item(0).href.slice(-1));
+	alert(i);
 	
 }
 //item(i).getElementsByTagName('a').item(0).href.slice(-1)
